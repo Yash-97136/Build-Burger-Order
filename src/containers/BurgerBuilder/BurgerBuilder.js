f8b5794000594a66b5886a5b222089f7e325f0ac
Yash-97136 <gudisayashwanth1@gmail.com>
2020-12-09 04:28:44 +0530

    add some code

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Auxiliary';
+
+class burgerBuilder extends Component {
+    render() {
+        return (
+            <Aux>
+                <p>Burger</p>
+                <p>Builder controls</p>
+            </Aux>
+        )
+    }
+}
+
+export default burgerBuilder;
